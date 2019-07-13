2768ab64d5e58fcf995858de9cae7c1b3b71d190
MonsterJwj <1404853538@qq.com>
2019-07-13 12:11:56 +0800

    更新的公共部分

--- src/assets/js/rem.js
+++
@@ -1,8 +0,0 @@
-export default{
-    setRem(){
-        var clientWidth=window.innerWidth;
-        var uiwidth  = 1440; // ui设计稿的宽度
-        var nowRem=(clientWidth/uiwidth*100);
-        document.querySelector('html').style.fontSize=parseInt(nowRem,10)+"px";
-    }
-}
